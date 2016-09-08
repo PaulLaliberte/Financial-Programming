51d87f1184e2724096b032fc3fc467ef4f788f9b
Paul Laliberte <Paul@rgnt1-33-9-dhcp.int.colorado.edu>
2016-09-07 20:32:25 -0600

    new project(s)

---
+++ c++/interest_rate_example/int_rate.cpp
@@ -0,0 +1,26 @@
+```cpp
+//
+//  int_rate.cpp
+//  Simple_int
+//
+//  Created by Paul Laliberte on 2/4/16.
+//  Copyright © 2016 Paul Laliberte. All rights reserved.
+//
+
+#include "int_rate.hpp"
+
+ir::ir(double rate) : m_rate(rate) {}
+
+ir::~ir() {}
+
+ir::ir(const ir &v) : m_rate(v.m_rate) {}
+
+ir &ir::operator=(const ir &v)
+{
+    if (&v != this)
+    {
+        this->m_rate = v.m_rate;
+    }
+    return *this;
+}
+```
